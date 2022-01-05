d0dcb465fccfa61a9fea8ce73c5652ceb9fe2599
Galdan Infotech <galdaninfotech@gmail.com>
2022-01-05 18:45:16 +0530

    first commit

---
+++ bookingmail.php
@@ -0,0 +1,29 @@
+<?php
+ob_start();
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$comments = $_POST['comments'];
+
+
+
+$message= <<<MSG
+Recently an online form is been filled out on Contact page of White Copper Travel with the following details.
+
+Name: $name
+Email: $email
+Phone: $phone
+Message: $comments
+
+Thanking You
+
+System Administrator
+MSG;
+
+mail("whitecoppertravel@gmail.com", "An Online Enquiry Form Filled", $message, "From: White Copper Travel<whitecoppertravel@gmail.com>");
+
+header("Location: thanks.html");
+exit;
+
+?>
